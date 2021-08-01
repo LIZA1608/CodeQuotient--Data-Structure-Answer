b727d265a1e11567c050d490bb45b4a54bbc36cd
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-08-01 19:22:49 +0530

    Create Count negative, positive, even and odd .cpp

---
+++ Arrays & Linear Search/Count negative, positive, even and odd .cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int main(){
+  int p,n,e,o;
+  p=n=e=o=0;
+  int arr[10];
+  for(int i=0;i<10;i++)
+  {
+    cin>>arr[i];
+  }
+  for(int i=0;i<10;i++)
+  {
+    if(arr[i]>0)
+    {
+      p=p+1;
+    }
+     if(arr[i]<0)
+    {
+      n=n+1;
+    }
+     if(arr[i]%2==0)
+    {
+      e=e+1;
+    }
+    else
+    {
+      o=o+1;
+    }
+  }
+  cout<<p<<"\n";
+    cout<<n<<"\n";
+    cout<<e<<"\n";
+    cout<<o<<"\n";
+  return 0;
+}
