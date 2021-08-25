866079344437973c071da396e143f8ddb396ce0a
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-08-25 18:22:40 +0530

    Create Implement the stack using Linked List.cpp

---
+++ Stack/Implement the stack using Linked List.cpp
@@ -0,0 +1,40 @@
+class LinkStack{
+  LinkList *first; // ref to first item on list
+  public:
+    LinkStack(){
+        first = NULL;
+    }
+    bool isEmpty(){
+      return first == NULL;
+    }
+    void push(int dd){
+		if(isEmpty()){
+          first = new LinkList(dd);
+        }
+      else{
+        LinkList* temp = first;
+        while(temp->next != NULL){
+          temp=temp->next;
+        }
+        temp->next = new LinkList(dd);
+      }
+    }
+    int pop(){
+		if(isEmpty()){
+          return -1;
+        } else{
+          if(first->next == NULL){
+            int res = first->data;
+            first = NULL;
+            return res;
+          }
+          LinkList* temp = first;
+          while(temp->next->next != NULL){
+            temp = temp->next;
+          }
+          int res = temp->next->data;
+          temp->next = NULL;
+          return res;
+        }
+    }
+};
