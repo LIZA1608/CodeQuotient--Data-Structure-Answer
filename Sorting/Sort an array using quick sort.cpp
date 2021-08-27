6807024cf400c8eb22501ae14df505e05ab36d3e
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-08-27 11:10:43 +0530

    Create Sort an array using quick sort.cpp

---
+++ Sorting/Sort an array using quick sort.cpp
@@ -0,0 +1,24 @@
+void swap(int array[], int i, int j){
+  int temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+int partition (int array[], int low, int high) {
+  int pivotInd = low;
+  int pivot=array[low];
+  for(int i=low+1; i<=high; i++){
+    if(array[i] < pivot){
+      swap(array, ++pivotInd, i);
+    }
+  }
+  swap(array, low, pivotInd);
+  return pivotInd;
+}
+/* low is for left index and high is right index of the sub-array of arr to be sorted */
+void quickSort(int array[], int low, int high) {
+  if(low < high){
+    int pivotInd = partition(array, low, high);
+    quickSort(array, low, pivotInd-1);
+    quickSort(array, pivotInd+1, high);
+  }
+}
