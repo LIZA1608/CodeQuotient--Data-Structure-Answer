4459be134a0a743692f6e5a91a29039ef32b0842
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-08-31 11:26:24 +0530

    Create Print nodes at odd levels of the binary tree.cpp

---
+++ Binary Tree/Print nodes at odd levels of the binary tree.cpp
@@ -0,0 +1,21 @@
+#include <queue>
+void printOdd(struct Node* root)
+{
+  if(root == NULL) return;
+  queue<Node*> q;
+  q.push(root);
+  struct Node* temp;
+  while(!q.empty()){
+    temp = q.front();
+    cout<<temp->data<<" ";
+    if(temp->left) {
+      if(temp->left->left) q.push(temp->left->left);
+      if(temp->left->right) q.push(temp->left->right);
+    };
+	if(temp->right) {
+      if(temp->right->left) q.push(temp->right->left);
+      if(temp->right->right) q.push(temp->right->right);
+    };
+    q.pop();
+  }
+}
